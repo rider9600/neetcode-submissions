218b728cb3739d6663709ab731ac0278ba8911f4
Naveen Kumar <medamnaveen16@gmail.com>
2026-06-22 11:08:40 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        string t = "";
+        for (char c : s) {
+            if (isalnum(c)) {
+                t += tolower(c);
+            }
+        }
+        int left = 0;
+        int right = t.size() - 1;
+        while (left < right) {
+            if (t[left] != t[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
